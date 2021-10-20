4d804b2b6a285c5e3ec519f3c6fc75f80945e1aa
Jader Martins <jadermcs94@gmail.com>
2021-10-20 18:53:26 -0300

    minor fixes

--- damd.py
+++ damd.py
@@ -8,16 +8,17 @@ counter = defaultdict(int)
 samples = []
 count = 0
 
-def dfs(flow, utterances=[]):
+def dfs(flow, dialog=[], num=0):
     global count
     if flow:
-        for utt in intent_sample[flow[0]]:
-            dfs(flow[1:], utterances+[utt])
-    elif random.random() > .9999:
+        for turn in intent_sample[flow[0]]:
+            turn["turn-num"] = num
+            dfs(flow[1:], dialog+[turn], num+1)
+    elif random.random() > .999:
         count += 1
         if count % 100 == 0:
             print(f"Generated {count} dialogues...")
-        samples.append(utterances)
+        samples.append(dialog)
 
 def main():
     possible_flows = set()
@@ -25,19 +26,29 @@ def main():
     with open("synthetic_anotado.json") as fin:
         data = json.load(fin)
     for dialog in data["dialogs"]:
+        if str(dialog["id"]).endswith(("1","2","3")): continue
         curr_flow = []
         for turn in dialog["turns"]:
             agent = "intent" if turn["turn-num"] % 2 == 0 else "action"
             utt = turn[agent]
             curr_flow.append(utt)
             counter[utt] += 1
-            intent_sample[utt].append(turn["utterance_delex"])
+            intent_sample[utt].append(turn)
         possible_flows.add(tuple(curr_flow))
     for i in counter.items():
         print(i)
 
     for flow in possible_flows:
         dfs(flow)
+
+    for i, dialog in enumerate(samples):
+        data["dialogs"].append({
+            "id": str(i+31),
+            "dialog_domain": "consulta_saldo",
+            "turns": dialog})
+    with open("synthetic_anotado_damd.json", "w") as fout:
+        json.dump(data, fout, indent=2, ensure_ascii=False)
+
 
 if __name__ == "__main__":
     main()
