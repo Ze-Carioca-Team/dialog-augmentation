d7ae115bc3c4a0f6e5a6db8d85efd6c68ac3084d
Jader Martins <jadermcs94@gmail.com>
2021-10-27 11:31:19 -0300

    update damd working

--- augment.py
+++ augment.py
@@ -5,7 +5,7 @@ import random
 augmented = {}
 random.seed(42)
 
-arquivo = open("/content/names_pt-br_new.json", "r", encoding='utf-8')
+arquivo = open("content/names_pt-br_new.json", "r", encoding='utf-8')
 names = json.load(arquivo)
 arquivo.close()
 
@@ -13,37 +13,29 @@ def default(o): # to save custom format in json=
     if type(o) is datetime.datetime:
         return o.isoformat()
 
-def gen_nome(old_name):
-    value = old_name[-1]
-    while (value in old_name):
-        gender = names[1][names[0].index(old_name[-1])]
-        index = random.randint(0, len(names[0])-1)
-        while(int(names[1][index]) != gender): index = random.randint(0, len(names[0])-1)
-        value = names[0][index]
+def gen_nome():
+    index = random.randint(0, len(names[0])-1)
+    value = names[0][index]
     return value
 
-def gen_placa(old_plate):
-    value = old_plate[-1]
-    while (value in old_plate):
-        value = ""
-        for i in range(3): value += string.ascii_letters[random.randint(0, len(string.ascii_letters)-1)].upper()
-        for i in range(4): value += str(random.randint(0, 9))
-        prob = random.random()
-        if (prob <= 0.6):
-            if (prob <= 0.2): value = value[:3]+" "+value[3:]
-            elif (prob <= 0.4): value = value[:3]+"-"+value[3:]
-        else: value = value[:4]+string.ascii_letters[random.randint(0, len(string.ascii_letters)-1)].upper()+value[5:]
+def gen_placa():
+    value = ""
+    for i in range(3): value += string.ascii_letters[random.randint(0, len(string.ascii_letters)-1)].upper()
+    for i in range(4): value += str(random.randint(0, 9))
+    prob = random.random()
+    if (prob <= 0.6):
+        if (prob <= 0.2): value = value[:3]+" "+value[3:]
+        elif (prob <= 0.4): value = value[:3]+"-"+value[3:]
+    else: value = value[:4]+string.ascii_letters[random.randint(0, len(string.ascii_letters)-1)].upper()+value[5:]
     return value
 
-def gen_cpf(old_cpf):
-    value = old_cpf[-1]
-    while (value in old_cpf):
-        value = ""
-        for i in range(11): value += str(random.randint(0, 9))
-        prob = random.randint(0,3)
-        if (prob==0): value = value[:3]+"."+value[3:6]+"."+value[6:9]+"-"+value[9:]
-        elif (prob==1): value = value[:3]+" "+value[3:6]+" "+value[6:9]+" "+value[9:]
-        elif (prob==2): value = value[:3]+""+value[3:6]+""+value[6:9]+"-"+value[9:]
+def gen_cpf():
+    value = ""
+    for i in range(11): value += str(random.randint(0, 9))
+    prob = random.randint(0,3)
+    if (prob==0): value = value[:3]+"."+value[3:6]+"."+value[6:9]+"-"+value[9:]
+    elif (prob==1): value = value[:3]+" "+value[3:6]+" "+value[6:9]+" "+value[9:]
+    elif (prob==2): value = value[:3]+""+value[3:6]+""+value[6:9]+"-"+value[9:]
     return value
 
 def gen_valor(old_value):
@@ -78,41 +70,42 @@ def order_dialog(dialog):
         result["turns"].append(new_turn)
     return result
 
-with open("synthetic_anotado.json", 'r', encoding='utf-8') as fin:
-    data = json.load(fin)
-    augmented["ontology"] = data["ontology"]
-    augmented["dialogs"] = []
-    for dialog in data["dialogs"]:
-        selected_values = {"cpf": [], "placa": [], "valor": [], "nome": []}
-        for i in range(random.randint(4, 20)):
-            new_dialog = dialog.copy()
-            new_dialog["id"] += i*100
-            new_dialog["turns"] = new_dialog["turns"].copy()
-            nome = None
-            for turn in new_dialog["turns"]:
-                turn["utterance"] = turn["utterance_delex"]
-                turn["slot-values"] = turn["slot-values"].copy()
-                if "[cpf]" in turn["utterance"]:
-                    selected_values["cpf"].append(turn["slot-values"]["cpf"])
-                    cpf = gen_cpf(selected_values["cpf"])
-                    turn["utterance"] = turn["utterance"].replace("[cpf]", cpf)
-                    turn["slot-values"]["cpf"] = cpf
-                if "[placa]" in turn["utterance"]:
-                    selected_values["placa"].append(turn["slot-values"]["placa"])
-                    placa = gen_placa(selected_values["placa"])
-                    turn["utterance"] = turn["utterance"].replace("[placa]", placa)
-                    turn["slot-values"]["placa"] = placa
-                if "[valor]" in turn["utterance"]:
-                    selected_values["valor"].append(turn["slot-values"]["valor"])
-                    valor = gen_valor(selected_values["valor"])
-                    turn["utterance"] = turn["utterance"].replace("[valor]", valor)
-                    turn["slot-values"]["valor"] = valor
-                if "[nome]" in turn["utterance"]:
-                    if (nome == None):
-                        selected_values["nome"].append(turn["slot-values"]["nome"])
-                        nome = gen_nome(selected_values["nome"])
-                    turn["utterance"] = turn["utterance"].replace("[nome]", nome)
-                    turn["slot-values"]["nome"] = nome
-            augmented["dialogs"].append(order_dialog(new_dialog))
-    with open("synthetic_augmented.json", 'w', encoding='utf-8') as fout:
-        fout.write(json.dumps(augmented, indent=2, default=default, ensure_ascii=False))
+if __name__ == "__main__":
+    with open("synthetic_anotado.json", 'r', encoding='utf-8') as fin:
+        data = json.load(fin)
+        augmented["ontology"] = data["ontology"]
+        augmented["dialogs"] = []
+        for dialog in data["dialogs"]:
+            selected_values = {"cpf": [], "placa": [], "valor": [], "nome": []}
+            for i in range(random.randint(4, 20)):
+                new_dialog = dialog.copy()
+                new_dialog["id"] += i*100
+                new_dialog["turns"] = new_dialog["turns"].copy()
+                nome = None
+                for turn in new_dialog["turns"]:
+                    turn["utterance"] = turn["utterance_delex"]
+                    turn["slot-values"] = turn["slot-values"].copy()
+                    if "[cpf]" in turn["utterance"]:
+                        selected_values["cpf"].append(turn["slot-values"]["cpf"])
+                        cpf = gen_cpf()
+                        turn["utterance"] = turn["utterance"].replace("[cpf]", cpf)
+                        turn["slot-values"]["cpf"] = cpf
+                    if "[placa]" in turn["utterance"]:
+                        selected_values["placa"].append(turn["slot-values"]["placa"])
+                        placa = gen_placa()
+                        turn["utterance"] = turn["utterance"].replace("[placa]", placa)
+                        turn["slot-values"]["placa"] = placa
+                    if "[valor]" in turn["utterance"]:
+                        selected_values["valor"].append(turn["slot-values"]["valor"])
+                        valor = gen_valor()
+                        turn["utterance"] = turn["utterance"].replace("[valor]", valor)
+                        turn["slot-values"]["valor"] = valor
+                    if "[nome]" in turn["utterance"]:
+                        if (nome == None):
+                            selected_values["nome"].append(turn["slot-values"]["nome"])
+                            nome = gen_nome()
+                        turn["utterance"] = turn["utterance"].replace("[nome]", nome)
+                        turn["slot-values"]["nome"] = nome
+                augmented["dialogs"].append(order_dialog(new_dialog))
+        with open("synthetic_augmented.json", 'w', encoding='utf-8') as fout:
+            fout.write(json.dumps(augmented, indent=2, default=default, ensure_ascii=False))
--- damd.py
+++ damd.py
@@ -1,9 +1,12 @@
 import json
 import random
+from tqdm import tqdm
 from pprint import pprint
 from collections import defaultdict
+import nlpaug.augmenter.word as naw
+from augment import gen_placa, gen_nome, gen_cpf
 
-intent_sample = defaultdict(list)
+intent_sample = {"intent":defaultdict(list), "action":defaultdict(list)}
 counter = defaultdict(int)
 samples = []
 count = 0
@@ -11,7 +14,8 @@ count = 0
 def dfs(flow, dialog=[], num=0):
     global count
     if flow:
-        for turn in intent_sample[flow[0]]:
+        agent = "intent" if num % 2 == 0 else "action"
+        for turn in intent_sample[agent][flow[0]]:
             turn["turn-num"] = num
             dfs(flow[1:], dialog+[turn], num+1)
     elif random.random() > .999:
@@ -27,23 +31,49 @@ def main():
         data = json.load(fin)
     for dialog in data["dialogs"]:
         dialog["id"] = str(dialog["id"])
-        if dialog["id"].endswith(("1","2","3")): continue
+        # if dialog["id"].endswith(("1","2","3")): continue
+        if dialog["id"].endswith(("2","3")): continue
         curr_flow = []
         for turn in dialog["turns"]:
             agent = "intent" if turn["turn-num"] % 2 == 0 else "action"
             utt = turn[agent]
             curr_flow.append(utt)
             counter[utt] += 1
-            intent_sample[utt].append(turn)
+            intent_sample[agent][utt].append(turn)
         possible_flows.add(tuple(curr_flow))
-    for i in counter.items():
-        print(i)
 
     for flow in possible_flows:
         dfs(flow)
-
+    model_name = "neuralmind/bert-large-portuguese-cased"
+    # TODO: fix tokenizer to not replace special tokens
+    aug1 = naw.SynonymAug(aug_src='wordnet', lang='por', aug_max=1,
+                          stopwords=["[cpf]", "[nome]", "[placa]"])
+    # aug1 = naw.ContextualWordEmbsAug(model_path=model_name, model_type="bert",
+    #                                  aug_max=1, stopwords=["[", "]", "cpf", "nome", "placa"])
     size = len(data["dialogs"])
-    for i, dialog in enumerate(samples):
+    for i, dialog in enumerate(tqdm(samples)):
+        selected_values = {"cpf": [], "placa": [], "valor": [], "nome": []}
+        for num, turn in enumerate(dialog):
+            # turn["utterance_delex"] = aug1.augment(turn["utterance_delex"])
+            turn["utterance"] = turn["utterance_delex"]
+            turn["slot-values"] = {}
+            nome = None
+            if "[cpf]" in turn["utterance"]:
+                cpf = gen_cpf()
+                selected_values["cpf"].append(cpf)
+                turn["utterance"] = turn["utterance"].replace("[cpf]", cpf)
+                turn["slot-values"]["cpf"] = cpf
+            if "[placa]" in turn["utterance"]:
+                placa = gen_placa()
+                selected_values["placa"].append(placa)
+                turn["utterance"] = turn["utterance"].replace("[placa]", placa)
+                turn["slot-values"]["placa"] = placa
+            if "[nome]" in turn["utterance"]:
+                if (nome == None):
+                    nome = gen_nome()
+                    selected_values["nome"].append(nome)
+                turn["utterance"] = turn["utterance"].replace("[nome]", nome)
+                turn["slot-values"]["nome"] = nome
         data["dialogs"].append({
             "id": f"{(i*10000)+(i%6)+4}",
             "dialog_domain": "consulta_saldo",
