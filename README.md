52a0083e675bc27e85a2ef35eb5f8c04bf97d88d
Jader Martins <jadermcs94@gmail.com>
2021-11-10 03:00:29 -0300

    final working

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # dialog-augmentation
+
+get word vectors
+```sh
+wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.pt.300.bin.gz
+gzip -d cc.pt.300.bin.gz
+```
+
+install requirements
+```sh
+pip install -r requirements
+```
+
+run mada with data aug
+```sh
+python mada.py
+```
--- mada.py
+++ mada.py
@@ -1,79 +1,81 @@
 import json
 import random
-from deanony import gen_placa, gen_cpf, gen_nome, gen_valor
+from eda import eda
 from tqdm import tqdm
 from collections import defaultdict
+from deanony import gen_placa, gen_cpf, gen_nome, gen_valor
 
 random.seed(20211109)
-intent_sample = {"intent":defaultdict(list), "action":defaultdict(list)}
-counter = defaultdict(int)
-samples = []
-count = 0
 
-def dfs(flow, dialog=[], num=0):
-    global count
-    if flow:
-        agent = "intent" if num % 2 == 0 else "action"
-        for turn in intent_sample[agent][flow[0]]:
-            if random.random() > .7: return
-            turn["turn-num"] = num
-            dfs(flow[1:], dialog+[turn], num+1)
-    count += 1
-    if count % 100 == 0:
-        print(f"Generated {count} dialogues...")
-    samples.append(dialog)
+def augment(sentence):
+    return eda(sentence, ["[cpf]", "[placa]", "[valor]"])
+
+def dfs(pflow, intent_sample):
+    count = 0
+    samples = []
+    visit = []
+    for flow in pflow:
+        visit.append((flow,[]))
+    while visit:
+        flow, dialog = visit.pop(0)
+        if flow:
+            agent = "intent" if len(flow) % 2 == 0 else "action"
+            for turn in intent_sample[agent][flow[0]]:
+                if random.random() > .93:
+                    visit.append((flow[1:], dialog+[turn]))
+        else:
+            count += 1
+            samples.append(dialog)
+            if count % 100 == 0: print(f"Generated {count} dialogues...")
+    return samples
 
 def main():
-    possible_flows = set()
-
+    possible_flows = []
+    intent_sample = {"intent":defaultdict(list), "action":defaultdict(list)}
     with open("dialogs.json") as fin:
         data = json.load(fin)
     for dialog in data["dialogs"]:
         dialog["id"] = str(dialog["id"])
-        if dialog["id"].endswith(("2","3")): continue
         curr_flow = []
         for turn in dialog["turns"]:
             agent = "intent" if turn["turn-num"] % 2 == 0 else "action"
             utt = turn[agent]
             curr_flow.append(utt)
-            counter[utt] += 1
             intent_sample[agent][utt].append(turn)
-        possible_flows.add(tuple(curr_flow))
-
-    for flow in possible_flows:
-        dfs(flow)
+        if curr_flow not in possible_flows:
+            possible_flows.append(curr_flow)
+    samples = dfs(possible_flows, intent_sample)
     size = len(data["dialogs"])
+    out_data = []
     for i, dialog in enumerate(tqdm(samples)):
         current_values = {}
-        for num, turn in enumerate(dialog):
-            turn["utterance"] = turn["utterance_delex"]
-            turn["slot-values"] = {}
-            nome = None
-            if "intent" in turn and "[negacao]" in turn["intent"]:
+        new_dialog = []
+        for turn in dialog:
+            new_turn = turn.copy()
+            if new_turn["speaker"] == "client":
+                aug_text = augment(new_turn["utterance_delex"].lower())
+                new_turn["utterance_delex"] = random.choice(aug_text)
+            new_turn["utterance"] = new_turn["utterance_delex"]
+            if "[negacao]" in new_turn.get("intent", ""):
                 current_values = {}
-            if "[cpf]" in turn["utterance"]:
+            if "[cpf]" in new_turn["utterance"]:
                 cpf = gen_cpf()
                 current_values["cpf"] = cpf
-                turn["utterance"] = turn["utterance"].replace("[cpf]", cpf)
-                turn["slot-values"]["cpf"] = cpf
-            if "[placa]" in turn["utterance"]:
+                new_turn["utterance"] = new_turn["utterance"].replace("[cpf]", cpf)
+            if "[placa]" in new_turn["utterance"]:
                 placa = gen_placa()
                 current_values["placa"] = placa
-                turn["utterance"] = turn["utterance"].replace("[placa]", placa)
-                turn["slot-values"]["placa"] = placa
-            if "[nome]" in turn["utterance"]:
-                if (nome == None):
-                    nome = gen_nome()
-                current_values["nome"] = nome
-                turn["utterance"] = turn["utterance"].replace("[nome]", nome)
-            if turn["speaker"] == "client":
-                turn["slot-values"] = current_values
-        data["dialogs"].append({
+                new_turn["utterance"] = new_turn["utterance"].replace("[placa]", placa)
+            if new_turn["speaker"] == "client":
+                new_turn["slot-values"] = current_values.copy()
+            new_dialog.append(new_turn)
+        out_data.append({
             "id": f"{size+i}",
             "dialog_domain": "consulta_saldo",
-            "turns": dialog})
+            "turns": new_dialog})
+        data["dialogs"] = out_data
     with open("dialogs.mada.json", "w") as fout:
-        json.dump(data, fout, indent=2, ensure_ascii=False)
+        json.dump(data, fout, indent=2, ensure_ascii=False, sort_keys=True)
 
 
 if __name__ == "__main__":
