e5ee86a9e86c72c89030e3035dd2b97122390b18
Jader Martins <jadermcs94@gmail.com>
2021-11-30 19:13:01 -0300

    Revert "Integration between current algorithms (#1)"
    This reverts commit ebd862bd6231d0a998ae06889ac18adfd48a7feb.

--- mada.py
+++ mada.py
@@ -1,105 +1,80 @@
-from collections import defaultdict
-import deanonymization as deanony
-from tqdm import tqdm
-import argparse
-import random
 import json
-import util
+import random
+from eda import eda
+from tqdm import tqdm
+from collections import defaultdict
+from deanony import gen_placa, gen_cpf, gen_nome, gen_valor
+
 random.seed(20211109)
 
-def get_ids(data):
-    ids = []
-    for dialog in data:
-        ids.append(dialog["id"])
-    return ids
+def augment(sentence):
+    return eda(sentence, ["[cpf]", "[placa]", "[valor]"])
 
-def bfs(pflow, intent_sample, ids, example, rate = 0.91, silence = False, mode = "test"):
+def bfs(pflow, intent_sample):
     count = 0
     samples = []
     visit = []
-
-    lastalg = util.algtest
-    if ("train" in mode): lastalg = util.algtrain
-
-    for flow in pflow: visit.append((flow,[]))
-    while (visit):
+    for flow in pflow:
+        visit.append((flow,[]))
+    while visit:
         flow, dialog = visit.pop(0)
-        if (flow):
-            agent = "intent" if (len(flow) % 2 == 0) else "action"
+        if flow:
+            agent = "intent" if len(flow) % 2 == 0 else "action"
             for turn in intent_sample[agent][flow[0]]:
-                if (random.random() > float(rate)):
+                if random.random() > .91:
                     visit.append((flow[1:], dialog+[turn]))
         else:
             count += 1
-            result = example.copy()
-
-            while (result["id"] in ids):
-                result["id"] = str(len(ids)+random.randint(0, len(ids)))
-                result["id"] += random.sample(lastalg, k=1)[0]
-            ids.append(result["id"])
-
-            result["turns"] = dialog
-            samples.append(result)
-            if ((count % 100 == 0)and(silence == False)):
-                print(f"Generated {count} dialogues...")
+            samples.append(dialog)
+            if count % 100 == 0: print(f"Generated {count} dialogues...")
     return samples
 
-def mada(data, rate = 0.91, silence = False, mode = "test"):
+def main():
     possible_flows = []
-    intent_sample = {"intent" : defaultdict(list), "action" : defaultdict(list)}
+    intent_sample = {"intent":defaultdict(list), "action":defaultdict(list)}
+    with open("dialogs.json") as fin:
+        data = json.load(fin)
     for dialog in data["dialogs"]:
         dialog["id"] = str(dialog["id"])
-        if (dialog["id"].endswith(util.algtest)):
-            continue
+        if dialog["id"].endswith(("1","2","3")): continue
         curr_flow = []
         for turn in dialog["turns"]:
-            agent = "intent" if (turn["turn-num"] % 2 == 0) else "action"
+            agent = "intent" if turn["turn-num"] % 2 == 0 else "action"
             utt = turn[agent]
             curr_flow.append(utt)
             intent_sample[agent][utt].append(turn)
-        if (curr_flow not in possible_flows): possible_flows.append(curr_flow)
+        if curr_flow not in possible_flows:
+            possible_flows.append(curr_flow)
+    samples = bfs(possible_flows, intent_sample)
+    size = len(data["dialogs"])
+    out_data = []
+    for i, dialog in enumerate(tqdm(samples)):
+        current_values = {}
+        for num, turn in enumerate(dialog):
+            turn["turn-num"] = num
+            if turn["speaker"] == "client":
+                aug_text = augment(turn["utterance_delex"].lower())
+                turn["utterance_delex"] = random.choice(aug_text)
+            turn["utterance"] = turn["utterance_delex"]
+            if "[cpf]" in turn["utterance"]:
+                cpf = gen_cpf()
+                current_values["cpf"] = cpf.replace(" ","").replace("-","").replace(".","")
+                turn["utterance"] = turn["utterance"].replace("[cpf]", cpf)
+            if "[placa]" in turn["utterance"]:
+                placa = gen_placa()
+                current_values["placa"] = placa.replace(" ","").replace("-","")
+                turn["utterance"] = turn["utterance"].replace("[placa]", placa)
+            if turn["speaker"] == "client":
+                turn["slot-values"] = current_values.copy()
+        out_data.append({
+            "id": f"{i*1000}",
+            "dialog_domain": "consulta_saldo",
+            "turns": dialog})
+    random.shuffle(out_data)
+    data["dialogs"] += out_data
+    with open("dialogs.mada.json", "w") as fout:
+        json.dump(data, fout, indent=2, ensure_ascii=False, sort_keys=True)
 
-    ids = get_ids(data["dialogs"])
-    if (len(ids) > 0):
-        samples = bfs(possible_flows, intent_sample, ids,
-                      data["dialogs"][0], rate, silence, mode)
-        result = data.copy()
-        for key in data.keys():
-            try: result[key] = data[key].copy()
-            except: continue
-        result["dialogs"] = samples.copy()
-    else: return data
-    return result
-
-def build_mada(data, mode, args):
-    data = mada(data, args.rate, args.silence, mode)
-    data = deanony.deanonymization(data, True)
-    data = util.fill_ontology(data)
-    random.shuffle(data["dialogs"])
-    util.format_jsonfile(data, args.filename, ".mada"+str(mode))
-    return data
-
-def parse_args():
-    parser = argparse.ArgumentParser(description="Applying MADA on a dialog dataset formatted in the MultiWOZ pattern.")
-    parser.add_argument("--filename", type=str, default="original.json", help="Path to dialogs dataset.")
-    parser.add_argument("--rate", type=str, default=0.91, help="Pruning probability in the MADA tree of possibilities.")
-    parser.add_argument("--silence", type=str, default=False, help="Keep algorithm muted (do not show outputs).")
-    return parser.parse_args()
-
-def main(args):
-    data = None
-    with open(args.filename) as fin: data = json.load(fin)
-
-    train, test = util.get_train_test(data)
-    train = build_mada(train, ".train", args)
-    test = build_mada(test, ".test", args)
-
-    data = util.join_train_test(train, test)
-    data = deanony.deanonymization(data, True)
-    data = util.fill_ontology(data)
-    random.shuffle(data["dialogs"])
-    util.format_jsonfile(data, args.filename, ".mada")
 
 if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+    main()
